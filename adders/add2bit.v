4ad65caaa069031c0f6b0e77b839d9cf8b3e41e6
llathasa-veleth <llathasa@outlook.com>
2021-09-12 15:28:54 +0300

    Improve adders

--- adders/add2bit.v
+++ adders/add2bit.v
@@ -5,8 +5,8 @@ module add2bit (input [1:0] a, b,
                 output [1:0] sum,
                 output c_out);
 
-   // use intermediate carry between adders.
+   // use intermediate carry wire between adders.
    wire c_b;
-   add1bit f0(a[0], b[0], 1'b0, sum[0], c_b);
-   add1bit f1(a[1], b[1], c_b, sum[1], c_out);
+   add1bit fa1_0(a[0], b[0], 1'b0, sum[0], c_b);
+   add1bit fa1_1(a[1], b[1], c_b, sum[1], c_out);
 endmodule // add2bit
--- adders/add4bit.v
+++ adders/add4bit.v
@@ -5,8 +5,8 @@ module add4bit (input [3:0] a, b,
                 output [3:0] sum,
                 output c_out);
 
-   // use intermediate carry between adders.
+   // use intermediate carry wire between adders.
    wire c_b;
-   add2bit f00(a[0+:2], b[0+:2], 1'b0, sum[0+:2], c_b);
-   add2bit f11(a[2+:2], b[2+:2], c_b, sum[2+:2], c_out);
+   add2bit fa2_0(a[0+:2], b[0+:2], 1'b0, sum[0+:2], c_b);
+   add2bit fa2_1(a[2+:2], b[2+:2], c_b, sum[2+:2], c_out);
 endmodule // add4bit
